fbaa321273f5243228f9416dfe404aef9cd80f71
Alexandru Axenia <94038640+AlexAxenia@users.noreply.github.com>
2023-05-15 18:17:11 +0300

    Add files via upload

---
+++ recognition.py
@@ -0,0 +1,24 @@
+import os
+import cv2
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+# Incarcarea modelului
+model = tf.keras.models.load_model('handwritten_digits.model')
+
+# Prezicerea imaginilor din fisier
+image_number = 1
+while os.path.isfile('digits/digit{}.png'.format(image_number)):
+    try:
+        img = cv2.imread('digits/digit{}.png'.format(image_number))[:, :, 0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print("Cifra este {}".format(np.argmax(prediction)))
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+        image_number += 1
+
+    except:
+        print("Nu s-a putut citi imaginea! Se trece la urmatoarea imagine...")
+        image_number += 1
